f8abeee2187cf829d2fd5610a2d08594da878358
saba javakhishvili <69425498+javaxa01@users.noreply.github.com>
2026-02-18 01:53:24 +0400

    Task swift update

--- TaskAppLastProject/Task.swift
+++ TaskAppLastProject/Task.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct Task: Identifiable, Codable {
+    let id: UUID
+    var title: String
+    var isCompleted: Bool
+
+    init(title: String) {
+        self.id = UUID()
+        self.title = title
+        self.isCompleted = false
+    }
+}
+
