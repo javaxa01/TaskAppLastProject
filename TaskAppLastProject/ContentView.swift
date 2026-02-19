57e5ccd0b498e8a94b0672458c485241cfcdae7a
saba javakhishvili <69425498+javaxa01@users.noreply.github.com>
2026-02-19 18:47:02 +0400

    ContentView

---
+++ TaskAppLastProject/ContentView.swift
@@ -0,0 +1,8 @@
+//
+//  ContentView.swift
+//  TaskAppLastProject
+//
+//  Created by Saba Javakhishvili on 19.02.26.
+//
+
+import Foundation
---
+++ TaskAppLastProject/ToDoItem.swift
@@ -0,0 +1,14 @@
+//
+//  ToDoItem.swift
+//  TaskAppLastProject
+//
+//  Created by Saba Javakhishvili on 19.02.26.
+//
+
+import Foundation
+
+struct ToDoItem: Identifiable {
+    let id = UUID()
+    var title: String
+    var isCompleted: Bool
+}
